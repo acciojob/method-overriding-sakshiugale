b45aa16fb6a29dfae956d222f7fcde43dfda850a
SAKSHI UGALE <sakshiugale452@gmail.com>
2023-08-05 12:59:43 +0530

    Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        B objB = new B();
+        String resultA = objB.meth();
+        System.out.println(resultA);
+
+    }
 }
