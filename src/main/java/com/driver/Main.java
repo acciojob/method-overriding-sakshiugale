af13eecb30ab929ed2aadd5560baf7861945aeac
SAKSHI UGALE <sakshiugale452@gmail.com>
2023-08-05 13:10:51 +0530

    Sucessfull

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,10 +1,24 @@
 package com.driver;
 
 public class Main {
-    public static void main(String[] args) {
-        B objB = new B();
-        String resultA = objB.meth();
-        System.out.println(resultA);
+    public static class A {
+        public  String meth() {
+            return "Invoking method from class A";
+        }
+    }
 
+    public static class B extends A {
+        public  String meth() {
+            return "Method is overridden in Extendend class B";
+        }
+    }
+
+    public static void main(String[] args) {
+        // Task 3
+        B obj = new B();
+
+        // Task 5
+        String result = obj.meth();
+        System.out.println(result);
     }
 }
